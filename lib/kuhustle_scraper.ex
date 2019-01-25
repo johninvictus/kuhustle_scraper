303809b55c33c6320c79b05d7d0685dfc0b40a06
johninvictus <invictusnitude@gmail.com>
2019-01-25 16:18:58 +0300

    Initial commit

---
+++ lib/kuhustle_scraper.ex
@@ -0,0 +1,18 @@
+defmodule KuhustleScraper do
+  @moduledoc """
+  Documentation for KuhustleScraper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KuhustleScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kuhustle_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule KuhustleScraperTest do
+  use ExUnit.Case
+  doctest KuhustleScraper
+
+  test "greets the world" do
+    assert KuhustleScraper.hello() == :world
+  end
+end
